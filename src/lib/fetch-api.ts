50041e6d5d3ac3fd474feb04ea7edf722f7ab142
Felipe Paul Martins <felipe@superhuit.ch>
2024-08-15 15:41:02 +0200

    refactor: restore try/catch in fetch-api

--- src/lib/fetch-api.ts
+++ src/lib/fetch-api.ts
@@ -28,37 +28,37 @@ const fetchAPI: FetchApiFuncType = async (query, options) => {
 	// // Debug performances
 	// const perfsId = fetchAPITester.markStart(`${type} - ${name}`);
 
-	// try {
-	const res = await fetch(endpoint, {
-		method: 'POST',
-		headers,
-		body: JSON.stringify({
-			query: dedupeFragments(query),
-			variables,
-		}),
-	});
+	try {
+		const res = await fetch(endpoint, {
+			method: 'POST',
+			headers,
+			body: JSON.stringify({
+				query: dedupeFragments(query),
+				variables,
+			}),
+		});
 
-	const { data, errors } = await res.json();
+		const { data, errors } = await res.json();
 
-	if (errors) {
-		const limit = '=================';
-		const sep = '-----------------';
-		const date = new Date().toISOString();
-		const vars = JSON.stringify(variables, null, 2);
-		const errs = errors
-			.map((e: any) => `   - ${e.message} [${e.extensions.category}]`)
-			.join('\n');
+		if (errors) {
+			const limit = '=================';
+			const sep = '-----------------';
+			const date = new Date().toISOString();
+			const vars = JSON.stringify(variables, null, 2);
+			const errs = errors
+				.map((e: any) => `   - ${e.message} [${e.extensions.category}]`)
+				.join('\n');
 
-		throw new Error(
-			`\n${limit}\nGraphQl API error\n${sep}\n== date: ${date}\n== errors: \n${errs}\n== query: ${name}\n== variables: ${vars}\n${limit}\n`
+			throw new Error(
+				`\n${limit}\nGraphQl API error\n${sep}\n== date: ${date}\n== errors: \n${errs}\n== query: ${name}\n== variables: ${vars}\n${limit}\n`
+			);
+		}
+		result = data;
+	} catch (errors) {
+		(Array.isArray(errors) ? errors : [errors]).map((err) =>
+			console.error(err.message)
 		);
 	}
-	result = data;
-	// } catch (errors) {
-	// 	(Array.isArray(errors) ? errors : [errors]).map((err) =>
-	// 		console.error(err.message)
-	// 	);
-	// }
 
 	// // Debug performances
 	// fetchAPITester.markEnd(perfsId);
