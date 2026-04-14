d0b6eb612f15a988952f927512213f762bd906ee
SnugleJuice <marionement@gmail.com>
2026-04-14 12:09:05 +0200

    ✨ feat: add base css

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+
+import '@/css/base/index.css';
 
 // Fonts
 const geistSans = Geist({
