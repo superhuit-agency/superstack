3943c597b4b4fc3ea964e52a8480aacb8aed0848
SnugleJuice <marionement@gmail.com>
2026-04-14 15:03:01 +0200

    ✨ feat: add core/pullquote

---
+++ next/src/components/core/Pullquote/index.tsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export default function Pullquote(props: PullquoteProps) {
+  return (
+    <blockquote className="supt-pullquote">
+      <p>{props.value}</p>
+      <cite>{props.citation}</cite>
+    </blockquote>
+  );
+}
---
+++ next/src/components/core/Pullquote/typings.d.ts
@@ -0,0 +1,6 @@
+interface PullquoteAttributes extends BlockAttributes {
+  value: string;
+  citation: string;
+}
+
+interface PullquoteProps extends PullquoteAttributes {}
