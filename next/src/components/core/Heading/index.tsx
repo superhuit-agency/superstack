0c1ae1cff859820c6f6e6a1f47a4a798d946f7e8
SnugleJuice <marionement@gmail.com>
2026-04-16 10:58:03 +0200

    💄 style: add text align support for core/heading

--- next/src/components/core/Heading/index.tsx
+++ next/src/components/core/Heading/index.tsx
@@ -2,14 +2,18 @@ import cx from 'classnames';
 
 import './styles.css';
 
-function Heading({ className = '', content, level }: HeadingProps) {
+function Heading({ className = '', content, level, textAlign }: HeadingProps) {
   const HTag = `h${level}` as 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
   if (!content) return null;
 
   return (
     <HTag
-      className={cx('supt-heading', className)}
+      className={cx('wp-block-heading', className, {
+        '-center': textAlign === 'center',
+        '-right': textAlign === 'right',
+        '-left': textAlign === 'left',
+      })}
       dangerouslySetInnerHTML={{ __html: content }}
     />
   );
