ea436e583fd0ea3b80950ce26d8540316cd18ac1
Charlotte Habre <charlotte@superhuit.ch>
2026-04-20 10:16:51 +0200

    feat: :sparkles: Unregister some unwanted core blocks from WP

--- wordpress/theme/src/admin/editor.ts
+++ wordpress/theme/src/admin/editor.ts
@@ -1,5 +1,13 @@
 /* JavaScript for the block editor */
 import { addFilter } from "@wordpress/hooks";
+import domReady from "@wordpress/dom-ready";
+import {
+  getBlockTypes,
+  unregisterBlockType,
+  getBlockVariations,
+  unregisterBlockVariation,
+} from "@wordpress/blocks";
+
 import * as filters from "@/components/filters";
 
 import "./editor.css";
@@ -11,3 +19,30 @@ for (const key in filters) {
     addFilter(filter.hook, filter.namespace, filter.callback);
   }
 }
+
+/**
+ * Excluded blocks from the editor.
+ */
+
+const excludedBlocks = ["core/more", "core/nextpage"];
+const excludedBlockGroups = ["yoast/", "yoast-seo/", "polylang/"];
+const allowedEmbedVariants = ["youtube", "vimeo"];
+
+domReady(() => {
+  // Unregister some core blocks
+  const blockTypes = getBlockTypes();
+  blockTypes.forEach((block: any) => {
+    if (!block?.name) return; // should not happen
+    if (excludedBlocks.includes(block.name)) unregisterBlockType(block.name);
+    excludedBlockGroups.forEach((group) => {
+      if (block.name.startsWith(group)) unregisterBlockType(block.name);
+    });
+  });
+
+  // Unregister embed variants that are not allowed
+  getBlockVariations("core/embed").forEach((variant) => {
+    if (!allowedEmbedVariants.includes(variant.name)) {
+      unregisterBlockVariation("core/embed", variant.name);
+    }
+  });
+});
